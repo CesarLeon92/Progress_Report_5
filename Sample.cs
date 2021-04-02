a3f7f0f548f9f6ec8adb8f55351bedf0f0d48482
CesarLeon92 <78234238+CesarLeon92@users.noreply.github.com>
2021-04-01 22:26:55 -0700

    Add files via upload
    This is some unity code I have been working on for my game.

---
+++ Sample.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static int score;
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            sceneManager.LoadScene(0);
+        }
+    }
+
+    public static int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            Debug.Log(score);
+            score = value;
+        }
+    }
+
+    public GameObject lemmingPrefab;
+    public Transform startPos;
+
+    public int amount;
+
+    public IEnumerator Spawn()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Instantiate(lemmingPrefab, startPos.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+}
---
+++ SpawnSample.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSample : MonoBehaviour
+
+
+
+
+{
+    Vector2 direction;
+    private Animator animation;
+    private Collider2D col;
+    private SpiritRenderer speedRend;
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(diresction * speed * Time.deltaTime);
+    }
+    
+    public void OnMouseDown()
+    {
+        GetBlock();
+    }
+
+    public void NextFlip()
+    {
+        speedRend.flipX = !speedRend.flipX;
+        direction.x = -1 * direction.x;
+    }
+
+    public void GetBlock()
+    {
+        gameObjectBlock = tag = "Object";
+        animation.SetTrigger("Object");
+        speed = 0;
+
+    }
+}
